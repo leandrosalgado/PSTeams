fc677125c53667a219d2ce2d05dc8c9196897920
Leandro Moreira Salgado <leandromsalgado@gmail.com>
2020-12-10 11:03:03 +0100

    Update User-Team script

--- Get-UserTeam.ps1
+++ Get-UserTeam.ps1
@@ -4,7 +4,7 @@ Connect-MicrosoftTeams
 
 $table = new-object System.Collections.ArrayList
 
-$userInfo = New-Object System.Collections.ArrayList
+$userInfo = @()
 
 $xlsxPath = "$PSScriptroot\List of BU Pigment Users_071220.xlsx"
 
@@ -12,6 +12,18 @@ foreach($row in (Import-XLSX -Path $xlsxPath -RowStart 1)) {
     $table.Add($row) | Out-Null
 }
 
-$table.mail | Select -Unique | ForEach-Object -Process { $userInfo.Add((Get-Team -User $_)) }
+foreach($user in ($table.mail | Select -Unique)) {
+    foreach($team in (Get-Team -User $user)) {
+        $userInfo += [PSCustomObject]@{
+          UserEmail   = $user
+          TeamID      = $team.GroupID
+          TeamName    = $team.DisplayName
+          Description = $team.Description
+        }
+    }
+}
 
 Disconnect-MicrosoftTeams
+
+$userInfo | Sort-Object UserEmail | Export-Csv -Path .\User-Teams.csv -Delimiter ";"
+
